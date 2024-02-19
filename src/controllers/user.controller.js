365a37da7e342ef84664ecd7e1b260b756cdd0e6
Hitesh <hiteshsharma7405@gmail.com>
2024-02-19 22:15:35 +0530

    added registeration api

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -21,7 +21,7 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "All fields are required");
 
     }
-    const existedUser = User.findOne({
+    const existedUser =  await User.findOne({
         $or: [{ email }, { username }]
     })
 
@@ -29,15 +29,29 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(409, "Username or email is existed")
     }
 
-    const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const avatarLocalPath =  req.files?.avatar[0].path;
+        console.log(req.files)
+    // const coverImageLocalPath =   req.files?.coverImage.path;
+// 
+
+// var avatarLocalPath;
+var coverImageLocalPath ;
+if(req.files && Array.isArray(req.files.avatar)&& req.files.avatar.length >0){
+    avatarLocalPath = req.files.avatar[0].path;
+}
+
+// let avatarLocalPath
+
+if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length >0){
+    coverImageLocalPath =req.files.coverImage[0].path
+}
 
     if (!avatarLocalPath) {
-        throw new ApiError(400, "Avatar file is required");
+        throw new ApiError(400, "Avatar file local path is missing");
     }
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
-
+console.log('value of avatar local path is given '+avatarLocalPath)
     if (!avatar) {
         throw new ApiError(400, "Avatar file is required");
     }
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,16 +1,24 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs"
 
-cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+
+// cloudinary.config({
+//     cloud_name: process.env.CLOUD_NAME,
+//     api_key: process.env.CLOUDINARY_API_KEY,
+//     api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+          
+cloudinary.config({ 
+  cloud_name: 'dfmlxo695', 
+  api_key: '969779955174411', 
+  api_secret: 'dpw_uA_6dWbf1CKEzhDFKen7vXQ' 
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
     try {
         if (!localFilePath) return null
         //upload the file on cloudinary
+        console.log('uploading !!!')
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
@@ -19,6 +27,8 @@ const uploadOnCloudinary = async (localFilePath) => {
         console.log(response);
         return response
     } catch (error) {
+        console.log('error while uploading');
+        console.log(error)
         fs.unlinkSync(localFilePath)
         //  it will remove the locally saved fule as operation got some probelm
         return null;
